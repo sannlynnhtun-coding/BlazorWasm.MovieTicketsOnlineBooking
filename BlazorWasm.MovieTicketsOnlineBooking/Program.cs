543cab26a3d0a3a96b7c7923ed769982d7336c7e
Phyu Phyu Thin <phyuphyuthin@acedatasystems.com>
2023-09-29 09:23:08 +0630

    [Added] MudBlazor

--- BlazorWasm.MovieTicketsOnlineBooking/Program.cs
+++ BlazorWasm.MovieTicketsOnlineBooking/Program.cs
@@ -2,10 +2,12 @@ using BlazorWasm.MovieTicketsOnlineBooking;
 using BlazorWasm.MovieTicketsOnlineBooking.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
