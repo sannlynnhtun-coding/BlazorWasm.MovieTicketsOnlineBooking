ca13439d4f915fe592bcc448cee085652745c88d
Phyu Phyu Thin <phyuphyuthin@acedatasystems.com>
2023-10-03 10:16:03 +0630

    -

---
+++ BlazorWasm.MovieTicketsOnlineBooking/Pages/PageRoomSeat.razor.cs
@@ -0,0 +1,45 @@
+﻿using BlazorWasm.MovieTicketsOnlineBooking.Models;
+using BlazorWasm.MovieTicketsOnlineBooking.Models.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorWasm.MovieTicketsOnlineBooking.Pages;
+
+public partial class PageRoomSeat
+{
+    [Parameter]
+    public CinemaRoomViewModel? Data { get; set; }
+
+    //public string? Data { get; set; }
+    private RoomDetailModel? _roomDetail = null;
+    private SeatNoModel? Seat = new();
+    private int singleCounter = 1;
+    private DateTime ShowDate { get; set; }
+    private List<BookingModel>? _bookingData = new();
+
+    protected override async Task OnParametersSetAsync()
+    {
+        if (Data is not null)
+            _roomDetail = await _dbService.GetRoomDetail(Data.RoomId);
+    }
+
+    /*model = BlazorWasm.MovieTicketsOnlineBooking.Models.ViewModels.RoomSeatViewModel
+    SeatId = 116
+    RoomId = 2
+    SeatNo = "28"
+    RowName = "C"
+    SeatType = "single"*/
+    async Task ToBookingList(RoomSeatViewModel model)
+    {
+        var data = model;
+        if (ShowDate == default(DateTime))
+        {
+            await _dbService.SetBookingList(data, ShowDate);
+            _bookingData = await _dbService.GetBookingList();
+        }
+    }
+
+    void SelectedShowDate(DateTime showDate)
+    {
+        ShowDate = showDate;
+    }
+}
